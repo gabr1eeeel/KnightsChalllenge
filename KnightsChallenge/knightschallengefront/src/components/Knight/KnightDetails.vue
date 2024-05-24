44548fe0da28adfe8a2ef9b8785607f858cacf44
Gabriel Lima <ggabrielantonioo@gmail.com>
2024-05-24 01:18:10 -0300

    docker fixes and improve component knightdetails

--- KnightsChallenge/knightschallengefront/src/components/Knight/KnightDetails.vue
+++ KnightsChallenge/knightschallengefront/src/components/Knight/KnightDetails.vue
@@ -1,13 +1,85 @@
 <template>
     <div v-if="knight">
         <v-card class="mx-auto" max-width="500">
-            <v-card-title class="purple white--text">
-                <span class="headline">Detalhes do Cavaleiro</span>
+            <v-card-title>
+                <h1>Detalhes do Cavaleiro</h1>
             </v-card-title>
             <v-card-text>
-                <v-row v-for="(value, key) in knight" :key="key">
+                <v-row>
                     <v-col cols="12">
-                        <p><b>{{ key }}</b>: {{ value }}</p>
+                        <p><b>Nome:</b> {{ knight.name }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Apelido:</b> {{ knight.nickname }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Aniversario:</b> {{ new Date(knight.birthday).toLocaleDateString('pt-BR') }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Atributo Principal:</b> {{ knight.keyAttribute }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Heroi:</b> {{ knight.isHero ? 'Sim' : 'Nao' }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Idade:</b> {{ knight.age }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Experiencia:</b> {{ knight.experience }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Ataque:</b> {{ knight.attack }}</p>
+                    </v-col>
+                    <v-col cols="12">
+                        <p><b>Quantidade de Armas:</b> {{ knight.weaponCount }}</p>
+                    </v-col>
+                    <v-col v-if="knight.weaponCount" cols="12">
+                        <h3>Armas:</h3>
+                        <v-list>
+                            <v-list-item v-for="(weapon, index) in knight.weapons" :key="index">
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ weapon.name }}</v-list-item-title>
+                                    <v-list-item-subtitle>
+                                        <b>Mod:</b> {{ weapon.mod }}, <b>Atributo:</b> {{ weapon.attr }}, <b>Equipado:</b> {{ weapon.equipped ? 'Sim' : 'Nao' }}
+                                    </v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </v-col>
+                    <v-col cols="12">
+                        <h3>Atributos:</h3>
+                        <v-list>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title><b>Forca:</b> {{ knight.attributes.strength }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title><b>Destreza:</b> {{ knight.attributes.dexterity }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title><b>Constituicao:</b> {{ knight.attributes.constitution }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title><b>Inteligencia:</b> {{ knight.attributes.intelligence }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title><b>Sabedoria:</b> {{ knight.attributes.wisdom }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title><b>Carisma:</b> {{ knight.attributes.charisma }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
                     </v-col>
                 </v-row>
             </v-card-text>
